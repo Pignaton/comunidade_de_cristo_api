25ffaa01671225ed5e187a040e5566856fc3915b
Pignaton <kpignaton@ymail.com>
2022-03-10 22:49:18 -0300

    Adicionado endpoint para listar visitantes

--- app/Http/Controllers/api/VisitanteController.php
+++ app/Http/Controllers/api/VisitanteController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pessoa;
 
 class VisitanteController extends Controller
 {
-    //
+    public function listaVisitante()
+    {
+        $array = ['error' => ''];
+
+        $pessoas = Pessoa::enderecoPessoa();
+        if($pessoas){
+            $array['pessoas'] = $pessoas;
+            return $array;
+        }
+
+        return $array;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,13 +25,16 @@ Route::get('/401', [LoginController::class, 'naoautorizado'])->name('login');
 Route::post('/auth/login', [LoginController::class, 'login']);
 Route::post('/registro', [CadastroController::class, 'registro']);
 
-Route::middleware('auth:api')->group(function(){
-    Route::get('/auth/validacao', [LoginController::class, 'validaToken']);
-    Route::get('/auth/logout', [LoginController::class, 'logout']);
+Route::get('lista/curso', [CadastroController::class, 'listaCurso']);
+Route::get('lista/campanha', [CadastroController::class, 'listaCampanha']);
 
-    Route::get('/lista-vistantes', [VisitanteController::class, 'listaVisitante']);
-    Route::delete('/lista-vistantes', [VisitanteController::class, 'deletaVisitante']);
-    Route::put('/lista-vistantes', [VisitanteController::class, 'atualizaVisitante']);
+Route::middleware('auth:api')->group(function(){
+    Route::post('/auth/validacao', [LoginController::class, 'validaToken']);
+    Route::post('/auth/logout', [LoginController::class, 'logout']);
+
+    Route::get('/lista-visitantes', [VisitanteController::class, 'listaVisitante']);
+    Route::delete('/lista-visitantes', [VisitanteController::class, 'deletaVisitante']);
+    Route::put('/lista-visitantes', [VisitanteController::class, 'atualizaVisitante']);
 });
 
 
